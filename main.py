7cc11379c664ebe76bca362626b5acf874d0eaad
RogerHoms <rhoms@novatilu.com>
2021-12-13 22:26:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+import simpy
+
+
+def main():
+    print("Welcome to our simulation! Peixeteria i carnisseria")
+    #create objects
+    #enter parameters
+    #execute
+    #show results
+
+if __name__ == "__main__":
+    main()
